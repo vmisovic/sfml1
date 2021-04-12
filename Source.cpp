f3e0c7de3b73c2472d3bf2726713c5634e2fce81
Vladimir Misovic <vladimir.misovic03@gmail.com>
2021-04-12 16:17:46 +0200

    0.1

--- Source.cpp
+++ Source.cpp
@@ -1,23 +1,40 @@
-
+#include <iostream>
 #include <SFML\Graphics.hpp>
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(400, 400), "Za mog brata Zaklopcinca!",sf::Style::Close | sf::Style::Titlebar);
-    sf::CircleShape shape(200.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
+    sf::RenderWindow prozor(sf::VideoMode(400, 400), "Za mog brata Zaklopcinca!",sf::Style::Close | sf::Style::Resize);
+    sf::RectangleShape pravougaonik(sf::Vector2f(100.0f,100.0f));
+    pravougaonik.setFillColor(sf::Color::Red);
+    while (prozor.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
+        sf::Event evnt;
+        while (prozor.pollEvent(evnt))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            prozor.clear(); switch (evnt.type)
+            {
+            case sf::Event::Closed:
+                prozor.close();
+                break;
+            case sf::Event::Resized:
+                std::cout << evnt.size.width<< " "<<evnt.size.height<<std::endl;
+                break;
+            case sf::Event::TextEntered:
+                if (evnt.text.unicode < 128)
+                    printf("%c", evnt.text.unicode);
+                break;
+            }
         }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+        {
+            pravougaonik.move(-0.1f, 0.0f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+        {
+            pravougaonik.move(0.1f, 0.0f);
+        }
+        prozor.draw(pravougaonik);
+        prozor.display();
+        prozor.clear();
     }
 	return 0;
 }
