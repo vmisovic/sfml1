1f90361611bf10c99f2a8f8a851ce98211744746
Vladimir Misovic <vladimir.misovic03@gmail.com>
2021-04-12 14:46:53 +0200

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,23 @@
+
+#include <SFML\Graphics.hpp>
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(400, 400), "Za mog brata Zaklopcinca!",sf::Style::Close | sf::Style::Titlebar);
+    sf::CircleShape shape(200.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+	return 0;
+}
